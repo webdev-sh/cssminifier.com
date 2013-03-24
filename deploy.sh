5ed04338eef84dfdf0308676e8a40b1d97b5e3c5
Andrew Chilton <chilts@appsattic.com>
2013-03-24 22:47:10 +1300

    Extra output in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,10 @@ npm install
 echo
 
 # minimising assets
+echo "Minimising assets ..."
 # ToDo: js-min.pl public/s/js/ready.js > public/s/js/ready.min.js
 # ToDo: curl -X POST -s --data-urlencode 'input@public/s/css/style.css' http://cssminifier.com/raw > public/s/css/style.min.css
+echo
 
 # set up Nginx
 echo "Setting up Nginx ..."
