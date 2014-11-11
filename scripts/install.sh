b5574de09967a1e6e20ea1a4a70d8e36b0d78ce2
Andrew Chilton <andychilton@gmail.com>
2014-11-11 23:47:55 +1300

    Make the install script nicer, and make the /var/lib/com-cssminifier directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,6 +11,10 @@ THIS_GROUP=`id -gn`
 THIS_PWD=`pwd`
 THIS_NODE=`which node`
 
+NAME=com-cssminifier
+NAKED_DOMAIN=cssminifier.com
+PORT=8011
+
 ## ----------------------------------------------------------------------------
 
 # install any required packages
@@ -34,40 +38,43 @@ echo
 
 # set up Nginx
 echo "Setting up Nginx ..."
-FILE=/tmp/com-cssminifier
+FILE=/tmp/$NAME
 cat /dev/null > $FILE
 nginx-generator \
-    --name com-cssminifier \
-    --domain cssminifier.com \
+    --name $NAME \
+    --domain $NAKED_DOMAIN \
     --type proxy \
     --var host=localhost \
-    --var port=8011 \
+    --var port=$PORT \
     - >> $FILE
 nginx-generator \
-    --name com-cssminifier-www \
-    --domain www.cssminifier.com \
+    --name $NAME-www \
+    --domain www.$NAKED_DOMAIN \
     --type redirect \
-    --var to=cssminifier.com \
+    --var to=$NAKED_DOMAIN \
     - >> $FILE
 nginx-generator \
-    --name com-cssminifier-ww \
-    --domain ww.cssminifier.com \
+    --name $NAME-ww \
+    --domain ww.$NAKED_DOMAIN \
     --type redirect \
-    --var to=cssminifier.com \
+    --var to=$NAKED_DOMAIN \
     - >> $FILE
 nginx-generator \
-    --name com-cssminifier-w \
-    --domain w.cssminifier.com \
+    --name $NAME-w \
+    --domain w.$NAKED_DOMAIN \
     --type redirect \
-    --var to=cssminifier.com \
+    --var to=$NAKED_DOMAIN \
     - >> $FILE
 sudo cp $FILE /etc/nginx/sites-enabled/
+rm -f $FILE
 echo
 
 # set up the server
 echo "Setting up various directories ..."
-sudo mkdir -p /var/log/com-cssminifier/
-sudo chown $THIS_USER:$THIS_GROUP /var/log/com-cssminifier/
+sudo mkdir -p /var/log/$NAME/
+sudo chown $THIS_USER:$THIS_GROUP /var/log/$NAME/
+sudo mkdir -p /var/lib/$NAME/
+sudo chown $THIS_USER:$THIS_GROUP /var/lib/$NAME/
 echo
 
 # add the supervisor scripts
@@ -76,7 +83,7 @@ m4 \
     -D __USER__=$THIS_USER \
     -D  __PWD__=$THIS_PWD  \
     -D __NODE__=$THIS_NODE \
-    etc/supervisor/conf.d/com-cssminifier.conf.m4 | sudo tee /etc/supervisor/conf.d/com-cssminifier.conf
+    etc/supervisor/conf.d/$NAME.conf.m4 | sudo tee /etc/supervisor/conf.d/$NAME.conf
 echo
 
 # restart services
